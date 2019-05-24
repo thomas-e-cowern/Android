8d7f5d989110329daec74263e90c8138ea13c0aa
Thomas E Cowern <thomas.e.cowern@gmail.com>
2019-05-24 11:46:26 -0400

    initial quiz app commit

--- ColorMyViews/app/src/main/java/com/example/colormyviews/MainActivity.kt
+++ ColorMyViews/app/src/main/java/com/example/colormyviews/MainActivity.kt
@@ -17,7 +17,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun setListeners () {
-        val clickableViews : List<View> = listOf(box_one_text, box_two_text, box_three_text, box_four_text, box_five_text, constraint_layout)
+        val clickableViews : List<View> = listOf(box_one_text, box_two_text, box_three_text, box_four_text, box_five_text, constraint_layout, button_one, button_two, button_three)
 
         for (item in clickableViews) {
             item.setOnClickListener { makeColored(it)}
@@ -35,6 +35,10 @@ class MainActivity : AppCompatActivity() {
             R.id.box_three_text -> view.setBackgroundResource(android.R.color.holo_green_light)
             R.id.box_four_text -> view.setBackgroundResource(android.R.color.holo_green_dark)
             R.id.box_five_text -> view.setBackgroundResource(android.R.color.holo_red_light)
+
+            R.id.button_one -> box_three_text.setBackgroundResource(R.color.my_red)
+            R.id.button_two -> box_four_text.setBackgroundResource(R.color.my_yellow)
+            R.id.button_three -> box_five_text.setBackgroundResource(R.color.my_green)
 
             else -> view.setBackgroundColor(Color.LTGRAY)
         }
---
+++ andfun-kotlin-android-trivia-starter-code/app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -0,0 +1,30 @@
+package com.example.android.navigation
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ *
+ */
+class TitleFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        return TextView(activity).apply {
+            setText(R.string.hello_blank_fragment)
+        }
+    }
+
+
+}
